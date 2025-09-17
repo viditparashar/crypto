536d71c7900672b6c0028b8e1f3032ba3b6eafea
Vidit Parashar <viditparashar@Vidis-MacBook-Pro.local>
2025-09-17 10:49:56 +0530

    first commit

---
+++ backend/src/test/java/com/crypto/trading/TradingApplicationTests.java
@@ -0,0 +1,20 @@
+package com.crypto.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=update"
+})
+class TradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // simple context start test
+    }
+}
